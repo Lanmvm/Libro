c88b7ab125abd2fbe05783201562dac9c49bb8b6
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 13:39:10 -0500

    Create Unidad_11_17_2.java

---
+++ Unidad_11_17_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Prueba de MarcoFlowLayout.
+import javax.swing.JFrame;
+ 
+public class Unidad_11_17_2
+ {
+ public static void main( String args[] )
+{
+    Unidad_11_17_1 marcoFlowLayout = new Unidad_11_17_1();
+ marcoFlowLayout.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+marcoFlowLayout.setSize( 350, 75 ); // establece el tamaño del marco
+marcoFlowLayout.setVisible( true ); // muestra el marco
+ } // fin de main
+} // fin de la clase DemoFlowLayout
