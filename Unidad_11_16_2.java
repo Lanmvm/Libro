7a736e7e2945e857896651db2ffad4a29e71f9b8
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 13:26:55 -0500

    Create Unidad_11_16_2.java

---
+++ Unidad_11_16_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Prueba de MarcoDemoTeclas.
+import javax.swing.JFrame;
+ 
+public class Unidad_11_16_2
+{
+public static void main( String args[] )
+{
+    Unidad_11_16_1 marcoDemoTeclas = new Unidad_11_16_1();
+marcoDemoTeclas.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+marcoDemoTeclas.setSize( 350, 100 ); // establece el tamaño del marco
+marcoDemoTeclas.setVisible( true ); // muestra el marco
+} // fin de main
+} // fin de la clase DemoTeclas
