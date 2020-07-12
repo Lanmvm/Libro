f18beccd2f59e51bf3ecdb9c46a1ff95400364ab
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 12:54:47 -0500

    Create Unidad_11_12_2.java

---
+++ Unidad_11_12_2.java
@@ -0,0 +1,14 @@
+package libro;
+// Prueba de MarcoSeleccionMultiple.
+import javax.swing.JFrame;
+ 
+public class Unidad_11_12_2
+{
+public static void main( String args[] )
+{
+    Unidad_11_12_1 marcoSeleccionMultiple =new Unidad_11_12_1();
+marcoSeleccionMultiple.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+marcoSeleccionMultiple.setSize( 350, 140 ); // establece el tamaño del marco
+marcoSeleccionMultiple.setVisible( true ); // muestra el marco
+}
+}
