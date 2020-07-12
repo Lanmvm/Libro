63408974f52eaf3413deff9b94a221ecd1da94df
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 13:51:11 -0500

    Create Unidad_11_19_2.java

---
+++ Unidad_11_19_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Copia el texto seleccionado de un área de texto a otra.
+ import javax.swing.JFrame;
+ 
+  public class Unidad_11_19_2
+  {
+  public static void main( String args[] )
+  {
+    Unidad_11_19_1 marcoAreaTexto = new Unidad_11_19_1();
+  marcoAreaTexto.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+  marcoAreaTexto.setSize( 425, 200 ); // establece el tamaño del marco
+ marcoAreaTexto.setVisible( true ); // muestra el marco
+} // fin de main
+ } // fin de la clase DemoAreaTexto
