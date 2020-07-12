c18663986f941cf91f1c192e8bf8d76e91383963
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 13:14:23 -0500

    Create Unidad_11_14_2.java

---
+++ Unidad_11_14_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Prueba de MarcoDetallesRaton.
+import javax.swing.JFrame;
+ 
+public class Unidad_11_14_2
+{
+public static void main( String args[] )
+{
+    Unidad_11_14_1 marcoDetallesRaton = new Unidad_11_14_1();
+marcoDetallesRaton.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+marcoDetallesRaton.setSize( 400, 150 ); // establece el tamaño del marco
+marcoDetallesRaton.setVisible( true ); // muestra el marco
+} // fin de main
+} // fin de la clase Unidad_11_14_2
