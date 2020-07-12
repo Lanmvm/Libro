c5f1c369171917608566f3c8c943663f55e2f1cd
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 13:47:56 -0500

    Create Unidad_11_18_2.java

---
+++ Unidad_11_18_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Prueba de MarcoPanel.
+import javax.swing.JFrame;
+ 
+public class Unidad_11_18_2 extends JFrame
+{
+public static void main( String args[] )
+{
+    Unidad_11_18_1 marcoPanel = new Unidad_11_18_1();
+marcoPanel.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+marcoPanel.setSize( 450, 200 ); // establece el tamaño del marco
+marcoPanel.setVisible( true ); // muestra el marco
+} // fin de main
+} // fin de la clase Unidad_11_18_2
