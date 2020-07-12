06ae86ff1b67fe5f882c81547b4541c6be9b77ee
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 13:10:57 -0500

    Create Unidad_11_13_2.java

---
+++ Unidad_11_13_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Prueba de MarcoRastreadorRaton.
+import javax.swing.JFrame;
+ 
+    public class Unidad_11_13_2
+{
+public static void main( String args[] )
+   {
+   Unidad_11_13_1 marcoRastreadorRaton = new Unidad_11_13_1();
+    marcoRastreadorRaton.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    marcoRastreadorRaton.setSize( 300, 100 ); // establece el tamaño del marco
+    marcoRastreadorRaton.setVisible( true ); // muestra el marco
+   } // fin de main
+} // fin de la clase Unidad_11_13_2
