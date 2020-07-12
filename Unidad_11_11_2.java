2bae6f8afc15006abbe5b6b8ce02a3276d4e631f
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 12:46:43 -0500

    Create Unidad_11_11_2.java

---
+++ Unidad_11_11_2.java
@@ -0,0 +1,15 @@
+package libro;
+
+// Selección de colores de un objeto JList.
+import javax.swing.JFrame;
+ 
+public class Unidad_11_11_2
+{
+public static void main( String args[] )
+{
+    Unidad_11_11_1 marcoLista = new Unidad_11_11_1(); // crea objeto MarcoLista
+marcoLista.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+marcoLista.setSize( 350, 150 ); // establece el tamaño del marco
+marcoLista.setVisible( true ); // muestra el marco
+} // fin de main
+} 
