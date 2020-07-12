cf77edff576b8ce16369586c2d47e72174b485ee
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 14:21:58 -0500

    Create Unidad_11_5_2.java

---
+++ Unidad_11_5_2.java
@@ -0,0 +1,13 @@
+package libro;
+// Prueba de CampoTextoMarco.
+import javax.swing.JFrame;
+
+public class Unidad_11_5_2 {
+    public static void main( String args[] )
+    {
+        Unidad_11_5_1 campoTextoMarco = new Unidad_11_5_1();
+        campoTextoMarco.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        campoTextoMarco.setSize( 350, 100 ); // establece el tamaño del marco
+        campoTextoMarco.setVisible( true ); // muestra el marco
+    }// fin de main
+} // fin de la clase PruebaCampoTexto
