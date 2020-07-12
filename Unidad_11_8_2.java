73a19532d132acc713046a41b2b1f96f37aea120
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 14:25:02 -0500

    Create Unidad_11_8_2.java

---
+++ Unidad_11_8_2.java
@@ -0,0 +1,12 @@
+package libro;
+import javax.swing.JFrame;
+
+public class Unidad_11_8_2 {
+        public static void main( String args[] )
+      {
+        Unidad_11_8_1 marcoBoton = new Unidad_11_8_1(); // crea MarcoBoton
+       marcoBoton.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+       marcoBoton.setSize( 300, 110 ); // establece el tamaño del marco
+      marcoBoton.setVisible( true ); // muestra el marco
+      } // fin de main
+}
