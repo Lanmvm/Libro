88b74e51568598a18eaa7efd004d8531c5a75216
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 12:47:48 -0500

    Update Unidad_11_10_2.java

--- Unidad_11_10_2.java
+++ Unidad_11_10_2.java
@@ -7,7 +7,7 @@ public class Unidad_11_10_2
 {
     public static void main( String args[] )
   {
-    Unidad_11_10_2 marcoCuadroCombinado = new Tema_11_10_A();
+    Unidad_11_10_1 marcoCuadroCombinado = new Unidad_11_10_1();
  marcoCuadroCombinado.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
  marcoCuadroCombinado.setSize( 350, 150 ); // establece el tamaño del marco
  marcoCuadroCombinado.setVisible( true ); // muestra el marco
