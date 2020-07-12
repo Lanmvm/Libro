3b7a12ede91ac6881f83c24c92931b40e4f83596
Lanmvm <60145391+Lanmvm@users.noreply.github.com>
2020-07-12 14:27:54 -0500

    Create Unidad_11_9_2.java

---
+++ Unidad_11_9_2.java
@@ -0,0 +1,13 @@
+package libro;
+// Prueba de MarcoCasillaVerificacion.
+import javax.swing.JFrame;
+
+public class Unidad_11_9_2 {
+    public static void main( String args[] )
+  {
+    Unidad_11_9_1 marcoCasillaVerificacion = new Unidad_11_9_1();
+  marcoCasillaVerificacion.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+  marcoCasillaVerificacion.setSize( 350, 100 ); // establece el tamaño del marco
+  marcoCasillaVerificacion.setVisible( true ); // muestra el marco
+  } // fin de main
+}
